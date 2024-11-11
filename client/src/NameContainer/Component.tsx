778419777503ee2e8687daab1ec27760c78f3bbc
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2024-11-11 20:21:22 +0200

    fetch function to call the api endpoint for age

--- client/src/NameContainer/Component.tsx
+++ client/src/NameContainer/Component.tsx
@@ -1,14 +1,34 @@
+import {useState, useEffect} from 'react';
 import './index.css'
 
 export default function NameContainer() {
+    const [age, setAge] = useState()
+    useEffect(() => {
+        fetch("http://localhost:5173/src/api/age.json")
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("invalid response")
+            }
+        })
+        .then((data) => {
+            const a = data.age;
+            setAge(a)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }, [])
     return (
         <>
         <div className="namecontainer">
             <div className="texts">
                 <h2>Aku Lehtola</h2>
-                <h4>self-taught software developer</h4>
+                <h4>{age}</h4>
             </div>
         </div>
         </>
     )
 }
+
