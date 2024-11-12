3609615dd0a41abeebb40d837aaa01fc46e37d68
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2024-11-12 22:30:16 +0200

    updated something

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import ProfilePictureContainer from './ProfilePictureContainer/Component'
 import NameContainer from './NameContainer/Component'
+import SocialsContainer from './SocialsContainer/Component'
 
 createRoot(document.getElementById('root')!).render(
     <div className="mainContainer">
       <ProfilePictureContainer/>
       <NameContainer />
+      <SocialsContainer />
     </div>
 )
