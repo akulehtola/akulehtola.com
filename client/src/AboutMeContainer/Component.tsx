ac9199d6f59cef5d230116b1e516dfd7338a4469
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2024-11-13 22:14:03 +0200

    added about me card

---
+++ client/src/AboutMeContainer/Component.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+export default function AboutMeContainer() {
+    return (
+        <div className="aboutmecontainer">
+            <div className="t">
+                <h2>About me</h2>
+                <h4>placeholder</h4>
+            </div>
+        </div>
+    )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,13 @@ import './index.css'
 import ProfilePictureContainer from './ProfilePictureContainer/Component'
 import NameContainer from './NameContainer/Component'
 import SocialsContainer from './SocialsContainer/Component'
+import AboutMeContainer from './AboutMeContainer/Component'
 
 createRoot(document.getElementById('root')!).render(
     <div className="mainContainer">
       <ProfilePictureContainer/>
       <NameContainer />
       <SocialsContainer />
+      <AboutMeContainer />
     </div>
 )
