f9de6da951dd9f663540f197403c334000b7bcbf
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2024-11-16 21:41:20 +0200

    added contact button to socialscontainer

--- client/src/SocialsContainer/Component.tsx
+++ client/src/SocialsContainer/Component.tsx
@@ -1,10 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import './index.css'
+
+function handleContact():void {
+    window.open("mailto:me@akulehtola.com")
+}
+
 
 export default function SocialsContainer() {
     return (
         <>
         <div className="socialscontainer">
-            
+            <button className="m" onClick={handleContact}>Contact me&nbsp;<FontAwesomeIcon icon={faEnvelope} /></button>
         </div>
         </>
     )
