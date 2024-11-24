9c2833b9d83b3109763a5700a10dd6dc2d6bb51d
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2024-11-24 21:39:38 +0200

    truncate text in about me card if too long, add a read more button to read the whole text

--- client/src/Components/AboutMeCard/Component.tsx
+++ client/src/Components/AboutMeCard/Component.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useEffect, useRef, useState } from 'react';
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './index.css'
 
 
-export default function AboutMeCard() {
+const AboutMeText = () => {
     const [about, setAbout] = useState<string | React.JSX.Element>("");
+    const [isTruncated, setIsTruncated] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+    
     useEffect(() => {
         const errText: string = "Error loading the text";
         const err: React.JSX.Element = <><FontAwesomeIcon icon={faTriangleExclamation}/> {errText}</>
@@ -13,14 +18,38 @@ export default function AboutMeCard() {
         .then((response) => response.json())
         .then((data) => setAbout(data.about))
         .catch(() => setAbout(err))
-    })
+    }, [ref])
+
+    useEffect(() => {
+        const div = ref.current;
+        if (div) {
+            setIsTruncated(div.scrollHeight > div.clientHeight)
+        }
+
+    }, [ref, about])
+
+    return (
+        <>
+            <div className="ref-div" ref={ref}>{about}</div>
+            {isTruncated && (
+                <div className="read-more" onClick={() => setIsExpanded(true)}>Read more</div>
+            )}
+            {isExpanded && (
+                ReactDOM.createPortal(<button onClick={() => setIsExpanded(false)}>Close</button>, document.body)
+            )}
+        </>
+    )
+}
+
+
+export default function AboutMeCard() {
     return (
         <div className="aboutmecard">
             <div className="t">
                 <h2 className="h">About me</h2>
             </div>
             <div className="textbox">
-                <h4 className="y">{about}</h4>
+                <h4 className="y"><AboutMeText /></h4>
             </div>
         </div>
     )
