a60050bf0670e89536506c5fc4226e52e75aa045
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2025-05-06 16:23:59 +0300

    added content to projects card

--- client/src/Components/ProjectsCard/Component.tsx
+++ client/src/Components/ProjectsCard/Component.tsx
@@ -7,7 +7,18 @@ export default function ProjectsCard() {
                 <h2 className={styles.title}>Projects</h2>
             </div>
             <div className={styles.content}>
-
+                <div className={styles.project}>
+                    <div className={styles.i}>
+                        <div className={styles.projectTitle}>
+                            <img alt="Logo" style={{margin: "2px"}} width="48" height="48" src="https://lattahattu.fi/lat.png"/><h3 style={{margin: "2px"}}>lattahattu.fi</h3>
+                        </div>
+                        <span className={styles.line}></span>
+                        <div className={styles.description}>
+                            <p className={styles.text}>A gallery website for Finnish railway enthusiasts</p>
+                            <button onClick={() => window.open("https://lattahattu.fi")} className={styles.button}>Visit</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
