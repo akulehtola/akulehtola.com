340d17b86f35d7c5f9f3de3d3a3a90eb8b456512
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2025-05-07 18:35:06 +0300

    added content to experience card

--- client/src/Components/ExperienceCard/Component.tsx
+++ client/src/Components/ExperienceCard/Component.tsx
@@ -6,6 +6,15 @@ export default function ExperienceCard() {
             <div className={styles.head}>
                 <h2 className={styles.title}>Experience</h2>
             </div>
+            <span className={styles.line}></span>
+            <div className={styles.content}>
+                <h2 className={styles.company}>Confirma Finland</h2>
+                <div className={styles.description}>
+                    <p className={styles.text}>Software Developer Intern</p>
+                    <p className={styles.text}>Turku, Finland &middot; June 2024 (2 weeks)</p>
+                    <p className={styles.text}>Worked with: TypeScript, Angular</p>
+                </div>
+            </div>
         </div>
     )
 }
