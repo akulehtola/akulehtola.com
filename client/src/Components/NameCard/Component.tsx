3948dabb11187a79c2b25e0ad152212151b56a20
Aku Lehtola <187870792+akulehtola@users.noreply.github.com>
2025-05-27 18:52:34 +0300

    made fonts more consistent across the page

--- client/src/Components/NameCard/Component.tsx
+++ client/src/Components/NameCard/Component.tsx
@@ -5,7 +5,7 @@ export default function NameCard() {
         <>
         <div className={styles.card}>
             <div className={styles.text}>
-                <h2 className={styles.content}>Aku Lehtola</h2>
+                <h2 className={styles.name}>Aku Lehtola</h2>
                 <h4 className={styles.content}>self-taught software developer from Southwest Finland</h4>
             </div>
         </div>
